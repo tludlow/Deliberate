23e329abc4b00ad0fc5e9f2010185c6e2103d622
tludlow <twjludlow@gmail.com>
2021-03-10 00:28:57 +0000

    Scheduling round 2

--- backend/src/lib/scheduling.ts
+++ backend/src/lib/scheduling.ts
@@ -15,45 +15,67 @@ export async function ScheduleDay(
     let endOfDay = dayjs(`${schedulingTime.format('YYYY-MM-DD')} 18:00:00`, 'YYYY-MM-DD HH:mm:ss')
     while (schedulingTime.isBefore(endOfDay) || schedulingTime.isSame(endOfDay)) {
         //Check if theres a task scheduled now
-        for (let [i, task] of daysTasks.entries()) {
-            let splitStart = task.start_time.split(':')
-            let splitEnd = task.end_time.split(':')
-            let startTime = dayjs(task.day)
+        while (true) {
+            if (tasksToSchedule.length == 0) {
+                console.log('nothing left to schedule')
+                return tasksToSchedule
+            }
+
+            if (Number(schedulingTime.hour()) >= 17 && Number(schedulingTime.minute()) > 0) {
+                //No time left in the day to schedule any more tasks
+                console.log('no time left for day')
+                return tasksToSchedule
+            }
+
+            let splitStart = daysTasks[0].start_time.split(':')
+            let splitEnd = daysTasks[0].end_time.split(':')
+            let startTime = dayjs(daysTasks[0].day)
                 .hour(splitStart[0])
                 .minute(splitStart[1])
                 .second(splitStart[2])
                 .millisecond(0)
-            let endTime = dayjs(task.day).hour(splitEnd[0]).minute(splitEnd[1]).second(splitEnd[2]).millisecond(0)
+            let endTime = dayjs(daysTasks[0].day)
+                .hour(splitEnd[0])
+                .minute(splitEnd[1])
+                .second(splitEnd[2])
+                .millisecond(0)
 
             //If a task is scheduled at this time jump to the end
-            if (
-                dayjs(task.day).isSame(schedulingTime, 'day') &&
-                schedulingTime.isBetween(startTime, endTime, null, '[)')
-            ) {
-                schedulingTime = schedulingTime
-                    .hour(endTime.hour())
-                    .minute(endTime.minute())
-                    .second(endTime.second())
-                    .millisecond(endTime.millisecond())
-                daysTasks = daysTasks.filter((innerTask, index) => innerTask.id !== task.id)
+            if (daysTasks.length > 0) {
+                if (schedulingTime.isBetween(startTime, endTime, null, '[)')) {
+                    console.log(`same as another task: ${schedulingTime.format('YYYY-MM-DD HH:mm:ss')}`)
+                    //Task already scheduled here, lets jump to the end of this task
+                    schedulingTime = schedulingTime
+                        .hour(endTime.hour())
+                        .minute(endTime.minute())
+                        .second(endTime.second())
+                        .millisecond(endTime.millisecond())
+                    daysTasks = daysTasks.filter((innerTask, index) => innerTask.id !== daysTasks[0].id)
+                    console.log(`jumped to: ${schedulingTime.format('YYYY-MM-DD HH:mm:ss')}`)
+                }
+            }
+
+            if (daysTasks.length > 0) {
                 let timeUntilNextTask = dayjs(daysTasks[0].day)
                     .hour(daysTasks[0].start_time.split(':')[0])
                     .minute(daysTasks[0].start_time.split(':')[1])
                     .second(daysTasks[0].start_time.split(':')[2])
                     .millisecond(0)
                     .diff(schedulingTime, 'hour', true)
-                if (
-                    schedulingTime.isAfter(
-                        dayjs(`${schedulingTime.format('YYYY-MM-DD')} 17:00:00`, 'YYYY-MM-DD HH:mm:ss')
-                    )
-                ) {
-                    //No time left in the day to schedule any more tasks
-                    return tasksToSchedule
-                }
+
                 if (timeUntilNextTask < 1) {
+                    console.log('no time to schedule here')
+                    //Jump to the next task so we can start scheduling again
+                    schedulingTime = dayjs(daysTasks[0].day)
+                        .hour(daysTasks[0].start_time.split(':')[0])
+                        .minute(daysTasks[0].start_time.split(':')[1])
+                        .second(daysTasks[0].start_time.split(':')[2])
+                        .millisecond(0)
+                    console.log(`jumping to: ${schedulingTime.format('YYYY-MM-DD HH:mm:ss')}`)
                     continue
                 } else {
                     //Schedule a task in this period
+                    console.log(`amount left to schedule: ${tasksToSchedule.length}`)
                     let schedulingIssue = tasksToSchedule[0]
                     let insertNewTask = await query(
                         'INSERT INTO tasks (title, description, day, start_time, end_time, calendar_id, type, issue_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
@@ -70,10 +92,32 @@ export async function ScheduleDay(
                     )
                     schedulingTime = schedulingTime.add(1, 'hour')
                     tasksToSchedule = tasksToSchedule.filter((taskSched, index) => taskSched.id !== schedulingIssue.id)
+                    console.log('-------------')
                 }
+            } else {
+                //Schedule a task in this period
+                console.log(`amount left to schedule: ${tasksToSchedule.length}`)
+                let schedulingIssue = tasksToSchedule[0]
+                let insertNewTask = await query(
+                    'INSERT INTO tasks (title, description, day, start_time, end_time, calendar_id, type, issue_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+                    [
+                        schedulingIssue.title,
+                        schedulingIssue.description,
+                        schedulingTime.format('YYYY-MM-DD'),
+                        `${schedulingTime.hour()}:${schedulingTime.minute()}:00`,
+                        `${schedulingTime.add(1, 'hour').hour()}:${schedulingTime.add(1, 'hour').minute()}:00`,
+                        user_id,
+                        'github',
+                        schedulingIssue.id,
+                    ]
+                )
+                schedulingTime = schedulingTime.add(1, 'hour')
+                tasksToSchedule = tasksToSchedule.filter((taskSched, index) => taskSched.id !== schedulingIssue.id)
+                console.log('-------------')
             }
         }
     }
+    console.log('escaping here')
     return tasksToSchedule
 }
 
@@ -94,7 +138,10 @@ export async function ScheduleUserCalendar(user_id: number) {
             //When to start scheduling (for today or tomorrow if the day has already ended)
             if (now.isAfter(dayjs().hour(18).minute(0).second(0))) {
                 //Start scheduling tomorrow (the day has already ended)
-                now = now.add(1, 'hour').hour(9).minute(0).second(0).millisecond(0)
+                now = now.add(1, 'day').hour(9).minute(0).second(0).millisecond(0)
+            }
+            if (now.isBefore(dayjs().hour(18).minute(0).second(0))) {
+                now = now.hour(9).minute(0).second(0).millisecond(0)
             }
 
             //Get the tasks for today
@@ -104,13 +151,12 @@ export async function ScheduleUserCalendar(user_id: number) {
             )
 
             let remainingIssues = await ScheduleDay(now, scheduledPersonalTasksForToday.rows, issuesLeft, user_id)
-            issuesToSchedule = remainingIssues
-
-            //Reset the day for tomorrows scheduling
-            now = now.add(1, 'hour').hour(9).minute(0).second(0).millisecond(0)
             if (issuesToSchedule.length === 0) {
                 break
             }
+            now = now.add(1, 'day').hour(9).minute(0).second(0).millisecond(0)
+            console.log(`remaining issues: ${remainingIssues.length}`)
+            issuesToSchedule = remainingIssues
         }
     } catch (error) {
         throw new Error(`Error scheduling tasks - ${error}`)
